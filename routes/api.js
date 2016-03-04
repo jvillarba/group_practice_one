c4d04e91f89ca9b84facbb3bf3e38457770ecd20
dona <dona.hsieh@gmail.com>
2016-03-04 15:51:12 -0800

    more routes added to api.js n index.js

--- routes/api.js
+++ routes/api.js
@@ -39,8 +39,37 @@ router.route('/users/:user_id')
   })
 })
 
+router.route('/users/user_id')
+
+.put(function(req, res){
+  User.findById(req.params.user_id, function(err, user){
+    if (err)
+      res.send(err)
+
+    user.name = req.body.name
+
+    user.save(function(err){
+      if (err)
+        res.send(err)
+      res.json({message: 'User updated!'})
+    })
+  })
+})
+
+router.route('/users/:user_id')
+
+.delete(function(req, res){
+  User.remove({_id: req.params.user_id}, function(err, user){
+    if (err)
+      res.send(err)
+    res.json({message: 'Successfully deleted'})
+  }
+  })
+})
 
 
 
+
+module.exports = router
 
 // app.use('/api', router)
