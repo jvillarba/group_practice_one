d4638bfd9120f0360fb992bdd78d44164719b9e2
Joe Villarba <socagambit@aol.com>
2016-03-04 15:04:14 -0800

    updated user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+// grab the things we need
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+// create a Schema
+var userSchema = new Schema({
+  name: String,
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String,
+  },
+  created_at: Date,
+  updated_at: Date
+})
+
+// create a model using a Schema
+var User = mongoose.model('User', userSchema)
+
+// make this available to our users in Node app
+module.exports = User
