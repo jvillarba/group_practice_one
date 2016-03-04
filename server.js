c43c9c8b511e19fcaec059620cfa80f412806e82
Nate Castor <nathancastor@gmail.com>
2016-03-04 14:46:05 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var mongoose = require('mongoose')
+var request = require('request')
+var bodyParser = require('body-parser');
+var app = express()
+
+
+
+
+app.listen(3000,function() {
+  console.log("app listening on port 3000");
+})
