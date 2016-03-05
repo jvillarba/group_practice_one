53ff9b83ddd0a365575a24cf42c787a3bb4b23fc
dona <dona.hsieh@gmail.com>
2016-03-04 16:33:11 -0800

    added require express and fixed brackets

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 ///////////  posts routes ///////////
-
+var express = require('express')
 var router = express.Router()
 
 router.use(function(req, res, next){
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 ///// users routes /////////////
+var express = require('express')
 var router = express.Router()
 
 router.use(function(req, res, next){
